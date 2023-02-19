d35bf60adf76ba8b224569f13341f21a5737a5ff
raoniton <raoniton@gmail.com>
2023-02-19 11:39:49 -0300

    Adicionando exercicio da Aula01

---
+++ java/exercicios_aula01/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+/**
+ *
+ * @author raoniton
+ */
+public class HelloWorld {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
