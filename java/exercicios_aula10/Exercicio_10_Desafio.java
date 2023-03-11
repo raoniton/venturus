90664e29100201178c5595ee727a4a3f1cf937bd
raoniton <raoniton@gmail.com>
2023-03-11 17:14:48 -0300

    Alteracao no exercicio_10 da aula 10

--- java/exercicios_aula10/Exercicio_10_Desafio.java
+++ java/exercicios_aula10/Exercicio_10_Desafio.java
@@ -18,23 +18,15 @@ public class Exercicio_10_Desafio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String cand1, cand2, cand3, cand4, cand5;
-        int nAlunos, op;
-        int opcao[] = new int[5], max;
-        int somatorio;
-        
+        String cand[];
+        cand = new String[5];
+        int nAlunos, op, i, opcao[] = new int[5], max, somatorio;
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Digite o nome do 1o candidato: ");
-        cand1 = scanner.nextLine();
-        System.out.println("Digite o nome do 2o candidato: ");
-        cand2 = scanner.nextLine();
-        System.out.println("Digite o nome do 3o candidato: ");
-        cand3 = scanner.nextLine();
-        System.out.println("Digite o nome do 4o candidato: ");
-        cand4 = scanner.nextLine();
-        System.out.println("Digite o nome do 5o candidato: ");
-        cand5 = scanner.nextLine();
+        for(i=0; i < 5; i++){
+            System.out.printf("Digite o nome do %do candidato: \n", i+1);
+            cand[i] = scanner.nextLine();
+        }
         
         System.out.printf("\nDigite o numero de alunos da sala: \n");
         nAlunos = scanner.nextInt();
@@ -42,7 +34,7 @@ public class Exercicio_10_Desafio {
         do{
             System.out.println("-------------------");
             System.out.println("Escolha o candidato: ");
-            System.out.printf("[1] - %s\n[2] - %s\n[3] - %s\n[4] - %s\n[5] - %s\n", cand1, cand2, cand3, cand4, cand5);
+            System.out.printf("[1] - %s\n[2] - %s\n[3] - %s\n[4] - %s\n[5] - %s\n", cand[0], cand[1], cand[2], cand[3], cand[4]);
             op = scanner.nextInt();
             switch(op){
                 case 1 -> opcao[0]=opcao[0]+1;
@@ -55,26 +47,20 @@ public class Exercicio_10_Desafio {
         somatorio = opcao[0] + opcao[1] + opcao[2] + opcao[3] + opcao[4];     
         }while(somatorio < nAlunos);
         
-        System.out.printf("Candidato[1] - %s -> %d votos \n"
-                + "Candidato[2] - %s -> %d votos \n"
-                + "Candidato[3] - %s -> %d votos \n"
-                + "Candidato[4] - %s -> %d votos \n"
-                + "Candidato[5] - %s -> %d votos \n", cand1, opcao[0], cand2, opcao[1], cand3, opcao[2], cand4, opcao[3], cand5, opcao[4]
-        );
+        
+        System.out.printf("-------------------\n");
+        for(i=0; i<5; i++){
+            System.out.printf("Candidato[%d] - %s -> %d votos \n", i+1, cand[i], opcao[i]);
+        }
         
         max = Math.max(opcao[0], Math.max(opcao[1], Math.max(opcao[2], Math.max(opcao[3], opcao[4]))));
         
         //Para evitar que caso tenha empate, so um dos candidatos seja mostrado, faco esse if que exibe todos candidatos que tenham empatado.        
-        if(opcao[0] == max)
-            System.out.printf("Candidato [1] - %s - Vencedor\n",cand1);
-        if(opcao[1] == max)
-            System.out.printf("Candidato [2] - %s - Vencedor\n",cand2);
-        if(opcao[2] == max)
-            System.out.printf("Candidato [3] - %s - Vencedor\n",cand3);
-        if(opcao[3] == max)
-            System.out.printf("Candidato [4] - %s - Vencedor\n",cand4);
-        if(opcao[4] == max)
-            System.out.printf("Candidato [5] - %s - Vencedor\n",cand5);
+        System.out.printf("-------------------\n");
+        for(i=0; i<5; i++){
+            if(opcao[i] == max)
+                System.out.printf("Candidato [%d] - %s - Vencedor\n",i+1,cand[i]);
+        }
         
     }
     
